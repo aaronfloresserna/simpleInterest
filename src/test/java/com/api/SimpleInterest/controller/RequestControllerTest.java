45502936d01af541b12563d061545639029e50be
aaronfloresserna <aaron.flores.serna@gmail.com>
2021-08-30 19:21:34 -0400

    test modified

--- src/test/java/com/api/SimpleInterest/controller/RequestControllerTest.java
+++ src/test/java/com/api/SimpleInterest/controller/RequestControllerTest.java
@@ -5,17 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -24,12 +20,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 
-import com.api.SimpleInterest.SimpleInterestApplication;
 import com.api.SimpleInterest.controllers.RequestController;
 import com.api.SimpleInterest.dto.SimpleInterestDTO;
 import com.api.SimpleInterest.models.RequestModel;
